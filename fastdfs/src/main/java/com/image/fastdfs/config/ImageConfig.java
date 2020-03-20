55841d53a15ec6d7aa83bb0713fb9ec13e07eb44
Mr.Kong <1031652818@qq.com>
2020-03-20 17:10:59 +0800

    update

---
+++ fastdfs/src/main/java/com/image/fastdfs/config/ImageConfig.java
@@ -0,0 +1,24 @@
+package com.image.fastdfs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+/**
+ * @Auther: Mr.Kong
+ * @Date: 2020/3/19 17:19
+ * @Description:
+ */
+@Configuration
+public class ImageConfig {
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(100 * 1024 * 1024);
+        multipartResolver.setDefaultEncoding("UTF-8");
+        return multipartResolver;
+    }
+
+}
